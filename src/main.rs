d81b482c7f7b2e831d7c989b6787261b07b47786
Jason Forral <JasonAForral@users.noreply.github.com>
2022-09-18 23:23:37 -0700

    added ui text

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::{
+    diagnostic::{FrameTimeDiagnosticsPlugin, Diagnostics},
     prelude::*,
+    sprite::MaterialMesh2dBundle,
     time::FixedTimestep,
-    sprite::MaterialMesh2dBundle
 };
 
 const TIME_STEP: f32 = 1.0 / 60.0;
@@ -10,7 +11,10 @@ const BOUNDS: Vec2 = Vec2::new(1200.0, 640.0);
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_startup_system(setup)
+        .add_system(text_update_system)
+        .add_system(text_color_system)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP as f64))
@@ -27,12 +31,21 @@ struct Player {
     movement_speed: f32,
     /// rotation speed in radians per second
     rotation_speed: f32,
-}   
+}
+
+// A unit struct to help identify the FPS UI component, since there may be many Text components
+#[derive(Component)]
+struct FpsText;
+
+// A unit struct to help identify the color-changing Text component
+#[derive(Component)]
+struct ColorText;
 
 fn setup (
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     commands.spawn_bundle(Camera2dBundle::default());
 
@@ -48,6 +61,57 @@ fn setup (
             movement_speed: 500.0,
             rotation_speed: f32::to_radians(360.0),
         });
+
+    commands
+        .spawn_bundle(
+            // Create a TextBundle that has a Text with a single section.
+            TextBundle::from_section(
+                // Accepts a `String` or any type that converts into a `String`, such as `&str`
+                "hello\nbevy!",
+                TextStyle {
+                    font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                    font_size: 100.0,
+                    color: Color::WHITE,
+                },
+            ) // Set the alignment of the Text
+            .with_text_alignment(TextAlignment::TOP_CENTER)
+            // Set the style of the TextBundle itself.
+            .with_style(Style {
+                align_self: AlignSelf::FlexEnd,
+                position_type: PositionType::Absolute,
+                position: UiRect {
+                    bottom: Val::Px(5.0),
+                    right: Val::Px(15.0),
+                    ..default()
+                },
+                ..default()
+            }),
+        )
+        .insert(ColorText);
+    commands
+        .spawn_bundle(
+            // Create a TextBundle that has a Text with a list of sections.
+            TextBundle::from_sections([
+                TextSection::new(
+                    "FPS: ",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 60.0,
+                        color: Color::WHITE,
+                    },
+                ),
+                TextSection::from_style(TextStyle {
+                    font: asset_server.load("fonts/FiraMono-Medium.ttf"),
+                    font_size: 60.0,
+                    color: Color::GOLD,
+                }),
+            ])
+            .with_style(Style {
+                align_self: AlignSelf::FlexEnd,
+                ..default()
+            }),
+        )
+        .insert(FpsText);
 }
 
 fn player_movement_system(
@@ -99,4 +163,34 @@ fn player_movement_system(
 
     transform.translation += translation_delta;
 
+}
+
+fn text_update_system(
+    diagnostics: Res<Diagnostics>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics.get(FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(average) = fps.average() {
+                text.sections[1].value = format!("{average:.2}")
+            }
+        }
+    }
+}
+
+fn text_color_system(
+    time: Res<Time>,
+    mut query: Query<&mut Text, With<ColorText>>,
+) {
+    for mut text in &mut query {
+        let seconds = time.seconds_since_startup() as f32;
+
+        // Update the color of the first and only section.
+        text.sections[0].style.color = Color::Rgba {
+            red: (1.25 * seconds).sin() / 2.0 + 0.5,
+            green: (0.75 * seconds).sin() / 2.0 + 0.5,
+            blue: (0.50 * seconds).sin() / 2.0 + 0.5,
+            alpha: 1.0,
+        };
+    }
 }
